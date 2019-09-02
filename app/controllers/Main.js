b5e112c0a20643b66e2896e2076d6f7949548c69
Addiel <addielJuarez@gmail.com>
2019-09-01 21:18:18 -0500

    se validan los 10 digitos en el switch

--- app/controllers/Main.js
+++ app/controllers/Main.js
@@ -35,7 +35,6 @@ if(hasStoragePerm == false){
  */
 function getContacts(_add52){
 
-    Ti.API.info('=== _add52 ===: ' + _add52);
 
     var people = Ti.Contacts.getAllPeople();
     var arrayContactos = [];
@@ -46,8 +45,6 @@ function getContacts(_add52){
             var row = Ti.UI.createTableViewRow({
                 height: Ti.UI.SIZE,
                 width: '100%',
-                borderColor: 'black',
-                borderWidth: 1,
                 layout: 'vertical',
                 data: people[i],
             });
@@ -69,25 +66,27 @@ function getContacts(_add52){
 
             if(people[i].phone.mobile != undefined){
                 for(j in people[i].phone.mobile){
+
+                    var valueTouchMobile = (deleteSpaces(people[i].phone.mobile[j]).length <= 9) ? false : true;
                     var viewPhone = Ti.UI.createView({
                         top: 5,
                         height: Ti.UI.SIZE,
-                        borderColor: 'blue',
-                        borderWidth: 1,
+                        touchEnabled: valueTouchMobile
                     });
                     var titlePhone = Ti.UI.createLabel({
-                        // text: 'Cel: ' + people[i].phone.mobile[j] + ' -> ' + deleteSpaces(people[i].phone.mobile[j]),
                         text: cleanOriginal(people[i].phone.mobile[j]) + ' -> ' + ((deleteSpaces(people[i].phone.mobile[j]).length < 10) ? deleteSpaces(people[i].phone.mobile[j]) : (((_add52 == true) ? '+52' : '') + deleteSpaces(people[i].phone.mobile[j]))),
                         top: 0,
                         left: 20,
                         height: Ti.UI.SIZE,
                         color: 'black',
+                        touchEnabled: false,
                     });
                     var switchView = Ti.UI.createSwitch({
                         right: 0,
-                        value:  (deleteSpaces(people[i].phone.mobile[j]).length < 10) ? false : true,
-                        touchEnabled: (deleteSpaces(people[i].phone.mobile[j]).length < 10) ? false : true,
+                        touchEnabled: false,
+                        value: valueTouchMobile,
                     });
+                    Ti.API.info('number: ' + deleteSpaces(people[i].phone.mobile[j]) + ', ' + valueTouchMobile);
                     viewPhone.add(titlePhone);
                     viewPhone.add(switchView);
                     row.add(viewPhone);      
@@ -96,38 +95,39 @@ function getContacts(_add52){
 
             if(people[i].phone.home != undefined){
                 for(j in people[i].phone.home){
+                    var valueTouchHome = (deleteSpaces(people[i].phone.home[j]).length <= 9) ? false : true;
                     var viewPhone = Ti.UI.createView({
                         top: 5,
                         height: Ti.UI.SIZE,
-                        borderColor: 'blue',
-                        borderWidth: 1,
+                        touchEnabled: valueTouchHome
                     });
                     var phoneHome = Ti.UI.createLabel({
-                        // text: 'home: ' + cleanOriginal(people[i].phone.home[j]) + ' -> ' + deleteSpaces(people[i].phone.home[j]),
                         text: cleanOriginal(people[i].phone.home[j]) + ' -> ' +     (( deleteSpaces(people[i].phone.home[j]).length < 10 ) ? deleteSpaces(people[i].phone.home[j]).length < 10 : ((_add52 == true) ? '+52' : '') + deleteSpaces(people[i].phone.home[j])),
                         top: 5,
                         left: 20,
                         height: Ti.UI.SIZE,
                         color: 'black',
+                        touchEnabled: false,
                     });
                     var switchView = Ti.UI.createSwitch({
                         right: 0,
-                        value: true,
+                        touchEnabled: false,
+                        value: valueTouchHome,
                     });
+                    Ti.API.info('number: ' + deleteSpaces(people[i].phone.home[j]) + ', ' + valueTouchHome);
                     viewPhone.add(phoneHome);
                     viewPhone.add(switchView);
                     row.add(viewPhone);   
-                    // row.add(phoneHome);   
                 }
             }
 
             if(people[i].phone.other != undefined){
                 for(j in people[i].phone.other){
+                    var valueTouchOther = (deleteSpaces(people[i].phone.other[j]).length <= 9) ? false : true;
                     var viewPhone = Ti.UI.createView({
                         top: 5,
                         height: Ti.UI.SIZE,
-                        borderColor: 'blue',
-                        borderWidth: 1,
+                        touchEnabled: valueTouchOther
                     });
                     var phoneOther = Ti.UI.createLabel({
                         // text: 'other: ' + cleanOriginal(people[i].phone.other[j])  + ' -> ' + deleteSpaces(people[i].phone.other[j]),
@@ -136,25 +136,27 @@ function getContacts(_add52){
                         left: 20,
                         height: Ti.UI.SIZE,
                         color: 'black',
+                        touchEnabled: false,
                     });
                     var switchView = Ti.UI.createSwitch({
                         right: 0,
-                        value: true,
+                        touchEnabled: false,
+                        value: valueTouchOther,
                     });
+                    Ti.API.info('number: ' + deleteSpaces(people[i].phone.other[j]) + ', ' + valueTouchOther);
                     viewPhone.add(phoneOther);
                     viewPhone.add(switchView);
-                    row.add(viewPhone);   
-                    // row.add(phoneOther);   
+                    row.add(viewPhone);    
                 }
             }
 
             if(people[i].phone.pager != undefined){
                 for(j in people[i].phone.pager){
+                    var valueTouchPager = (deleteSpaces(people[i].phone.pager[j]).length <= 9) ? false : true;
                     var viewPhone = Ti.UI.createView({
                         top: 5,
                         height: Ti.UI.SIZE,
-                        borderColor: 'blue',
-                        borderWidth: 1,
+                        touchEnabled: valueTouchPager
                     });
                     var phonePager = Ti.UI.createLabel({
                         // text: 'pager: ' + cleanOriginal(people[i].phone.pager[j]) + ' -> ' + deleteSpaces(people[i].phone.pager[j]),
@@ -163,25 +165,27 @@ function getContacts(_add52){
                         left: 20,
                         height: Ti.UI.SIZE,
                         color: 'black',
+                        touchEnabled: false,
                     });
                     var switchView = Ti.UI.createSwitch({
                         right: 0,
-                        value: true,
+                        touchEnabled: false,
+                        value: valueTouchPager,
                     });
+                    Ti.API.info('number: ' + deleteSpaces(people[i].phone.pager[j]) + ', ' + valueTouchPager);
                     viewPhone.add(phonePager);
                     viewPhone.add(switchView);
-                    row.add(viewPhone);     
-                    // row.add(phonePager);   
+                    row.add(viewPhone);
                 }
             }
 
             if(people[i].phone.work != undefined){
                 for(j in people[i].phone.work){
+                    var valueTouchWork = (deleteSpaces(people[i].phone.work[j]).length <= 9) ? false : true;
                     var viewPhone = Ti.UI.createView({
                         top: 5,
                         height: Ti.UI.SIZE,
-                        borderColor: 'blue',
-                        borderWidth: 1,
+                        touchEnabled: valueTouchWork
                     });
                     var phoneWork = Ti.UI.createLabel({
                         // text: 'work: ' + cleanOriginal(people[i].phone.work[j]) + ' -> ' + deleteSpaces(people[i].phone.work[j]),
@@ -190,25 +194,27 @@ function getContacts(_add52){
                         left: 20,
                         height: Ti.UI.SIZE,
                         color: 'black',
+                        touchEnabled: false,
                     });
                     var switchView = Ti.UI.createSwitch({
                         right: 0,
-                        value: true,
+                        touchEnabled: false,
+                        value: valueTouchWork,
                     });
+                    Ti.API.info('number: ' + deleteSpaces(people[i].phone.work[j]) + ', ' + valueTouchWork);
                     viewPhone.add(phoneWork);
                     viewPhone.add(switchView);
                     row.add(viewPhone);
-                    // row.add(phoneWork);   
                 }
             }
 
             if(people[i].phone.workFax != undefined){
                 for(j in people[i].phone.workFax){
+                    var valueTouchWorkFax = (deleteSpaces(people[i].phone.workFax[j]).length <= 9) ? false : true;
                     var viewPhone = Ti.UI.createView({
                         top: 5,
                         height: Ti.UI.SIZE,
-                        borderColor: 'blue',
-                        borderWidth: 1,
+                        touchEnabled: valueTouchWorkFax
                     });
                     var phoneWorkFax = Ti.UI.createLabel({
                         // text: 'workFax: ' + cleanOriginal(people[i].phone.workFax[j]) + ' -> ' + deleteSpaces(people[i].phone.workFax[j]),
@@ -217,25 +223,27 @@ function getContacts(_add52){
                         left: 20,
                         height: Ti.UI.SIZE,
                         color: 'black',
+                        touchEnabled: false,
                     });
                     var switchView = Ti.UI.createSwitch({
                         right: 0,
-                        value: true,
+                        touchEnabled: false,
+                        value: valueTouchWorkFax,
                     });
+                    Ti.API.info('number: ' + deleteSpaces(people[i].phone.workFax[j]) + ', ' + valueTouchWorkFax);
                     viewPhone.add(phoneWorkFax);
                     viewPhone.add(switchView);
                     row.add(viewPhone);
-                    // row.add(phoneWorkFax);
                 }
             }
 
             if(people[i].phone.homeFax != undefined){
                 for(j in people[i].phone.homeFax){
+                    var valueTouchHomeFax = (deleteSpaces(people[i].phone.homeFax[j]).length <= 9) ? false : true;
                     var viewPhone = Ti.UI.createView({
                         top: 5,
                         height: Ti.UI.SIZE,
-                        borderColor: 'blue',
-                        borderWidth: 1,
+                        touchEnabled: valueTouchHomeFax
                     });
                     var phoneHomeFax = Ti.UI.createLabel({
                         // text: 'homeFax: ' + cleanOriginal(people[i].phone.homeFax[j]) + ' -> ' + deleteSpaces(people[i].phone.homeFax[j]),
@@ -244,15 +252,17 @@ function getContacts(_add52){
                         left: 20,
                         height: Ti.UI.SIZE,
                         color: 'black',
+                        touchEnabled: false,
                     });
                     var switchView = Ti.UI.createSwitch({
                         right: 0,
-                        value: true,
+                        touchEnabled: false,
+                        value: valueTouchHomeFax,
                     });
+                    Ti.API.info('number: ' + deleteSpaces(people[i].phone.homeFax[j]) + ', ' + valueTouchHomeFax);
                     viewPhone.add(phoneHomeFax);
                     viewPhone.add(switchView);
                     row.add(viewPhone);
-                    // row.add(phoneHomeFax);   
                 }
             }
             
@@ -311,11 +321,6 @@ function cleanOriginal(_number){
     str = str = str.replace("(",''); 
     str = str = str.replace(")",''); 
     str = str = str.replace("-",''); 
-
-    // if(str.length > 10){
-    //     str = str.substring( (Number(str.length) - 10) , str.length);
-    //     str = str + ' si';
-    // }
 
     return str ;
 }
